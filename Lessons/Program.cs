5fcd8533c569ffc69272b37d22da8c472bebf990
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-07 12:32:14 +0200

    ReadLine input

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -7,18 +7,10 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            int a = 1, b = 2;
-            float f = 1.0f;
-            double d = 1.0;
-            char c = 'a';
-            string s = "Hello, World!";
+            string input; 
+            input = Console.ReadLine();
 
-            Console.WriteLine("Integer: " + a);
-            Console.WriteLine("Integer: " + b);
-            Console.WriteLine("Float: " + f);
-            Console.WriteLine("Double: " + d);
-            Console.WriteLine("Character: " + c);
-            Console.WriteLine("String: " + s);
+            Console.WriteLine("Your message is \"" + input + "\"");
         }
     }
 }
