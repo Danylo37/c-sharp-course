7a7e412dff6f192ba7216555eeaaf0fb3873760d
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-07 12:24:43 +0200

    variables

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -7,7 +7,18 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 1, b = 2;
+            float f = 1.0f;
+            double d = 1.0;
+            char c = 'a';
+            string s = "Hello, World!";
+
+            Console.WriteLine("Integer: " + a);
+            Console.WriteLine("Integer: " + b);
+            Console.WriteLine("Float: " + f);
+            Console.WriteLine("Double: " + d);
+            Console.WriteLine("Character: " + c);
+            Console.WriteLine("String: " + s);
         }
     }
 }
